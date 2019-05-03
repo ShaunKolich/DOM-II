16d7f4f9665bba5ada3c93251d91dbeab3ad1184
ShaunKolich <employmeinc@gmail.com>
2019-05-02 22:31:43 -0400

    Completed the Navigation Menu and attached it to Lambda Website

--- js/index.js
+++ js/index.js
@@ -4,16 +4,26 @@
 let aboutUS = document.querySelectorAll('nav');
 let test = aboutUS[0].getElementsByTagName('a')[1];
 test.addEventListener('click', function (e) {
-    console.log('hello');
     window.location.replace('https://lambdaschool.com/about/');
 });
 
 //ContactUs link to Lambda Apply
 
 test = aboutUS[0].getElementsByTagName('a')[3];
-console.log(test);
-test.addEventListener('click', function(e){
-    console.log('hello');
+test.addEventListener('click', function (e) {
     window.location.replace('https://lambdaschool.com/apply/');
+
+})
+
+//Home Page will link to Lambda School
+test = aboutUS[0].getElementsByTagName('a')[0];
+test.addEventListener('click', function (e) {
+    window.location.replace('https://lambdaschool.com/');
+
+})
+
+test = aboutUS[0].getElementsByTagName('a')[2];
+test.addEventListener('click', function (e) {
+    window.location.replace('https://lambdaschool.com/blog/');
 
 })
