13859e1a7ba913fdc99ac8a9b8ab9a64efa02b86
ShaunKolich <employmeinc@gmail.com>
2019-05-02 22:17:17 -0400

    Attached the about us to the about page on Lambda School website using event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+//About Us Link to Lambda
+let aboutUS = document.querySelectorAll('nav');
+let test = aboutUS[0].getElementsByTagName('a')[1];
+console.log(test);
+test.addEventListener('click', function (e) {
+    console.log('hello');
+    window.location.replace('https://lambdaschool.com/about/');
+});
