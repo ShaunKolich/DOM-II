4e47257cd13b5b10138e5155a8a941bcd8f16a1a
ShaunKolich <employmeinc@gmail.com>
2019-05-02 22:23:18 -0400

    Attached the contact link to Apply at Lambda Hompage

--- js/index.js
+++ js/index.js
@@ -3,8 +3,17 @@
 //About Us Link to Lambda
 let aboutUS = document.querySelectorAll('nav');
 let test = aboutUS[0].getElementsByTagName('a')[1];
-console.log(test);
 test.addEventListener('click', function (e) {
     console.log('hello');
     window.location.replace('https://lambdaschool.com/about/');
 });
+
+//ContactUs link to Lambda Apply
+
+test = aboutUS[0].getElementsByTagName('a')[3];
+console.log(test);
+test.addEventListener('click', function(e){
+    console.log('hello');
+    window.location.replace('https://lambdaschool.com/apply/');
+
+})
